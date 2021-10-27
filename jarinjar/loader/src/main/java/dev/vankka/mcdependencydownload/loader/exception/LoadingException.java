90df46e0e028ba6b7af783538582c3fbffe3190d
Vankka <vankka.main@gmail.com>
2021-10-27 19:52:54 +0300

    Move some parts to parent project

--- jarinjar/loader/src/main/java/dev/vankka/mcdependencydownload/loader/exception/LoadingException.java
+++
@@ -1,8 +0,0 @@
-package dev.vankka.mcdependencydownload.loader.exception;
-
-public class LoadingException extends RuntimeException {
-
-    public LoadingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
